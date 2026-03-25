c229488a3ef9dcdffbcbb6672bb01a75a58878fc
davidtav <david.mclaurel@outlook.com.br>
2026-03-24 21:33:13 -0300

    refactor: adicionar a palavra-chave 'required' às propriedades Name e Country da classe Colaborador

--- apiDotNet/CrudComAngular/Models/Colaborador.cs
+++ apiDotNet/CrudComAngular/Models/Colaborador.cs
@@ -3,7 +3,7 @@ namespace CrudComAngular.Models
     public class Colaborador
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Country { get; set; }
+        public required string Name { get; set; }
+        public required string Country { get; set; }
     }
-}
+} 
